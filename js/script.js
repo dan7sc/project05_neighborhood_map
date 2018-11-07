2b07f9356272c26bedae05e473957d970ac9218c
daniel sc <su7cheng@gmail.com>
2018-11-07 14:56:50 -0200

    Create and show all markers.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 
       var map;
+
+      // Create a new blank array for all markers.
+      var markers = [];
+
       function initMap() {
         // Constructor creates a new map
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 40.7413549, lng: -73.9980244},
           zoom: 13
         });
+		// Create an array of markers on initialize.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          console.log(title);
+          // Create a marker per location, and put into markers array.
+           var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+        }
+        showMarkers();
+      }
+
+      // This function will loop through the markers array and display them all.
+      function showMarkers() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
       }
