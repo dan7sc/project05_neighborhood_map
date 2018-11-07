7643359d2972da5a56031c85e758bc9f2a46cedc
daniel sc <su7cheng@gmail.com>
2018-11-07 14:55:25 -0200

    Add model, knockout scripts.

---
+++ js/model.js
@@ -0,0 +1,45 @@
+/* Model */
+var locations = [
+    {
+        title: 'Empire State Building',
+        location: {
+            lat: 40.748441,
+            lng: -73.985664
+        }
+    },
+    {
+        title: 'Rockefeller Center',
+        location: {
+            lat: 40.758663,
+            lng: -73.981331
+        }
+    },
+    {
+        title: 'Central Park Zoo',
+        location: {
+            lat: 40.767778,
+            lng: -73.971833
+        }
+    },
+    {
+        title: 'Times Square',
+        location: {
+            lat: 40.758895,
+            lng: -73.985131
+        }
+    },
+    {
+        title: 'Japan Society',
+        location: {
+            lat: 40.752524,
+            lng: -73.968287
+        }
+    },
+    {
+        title: 'Manhattan Bridge',
+        location: {
+            lat: 40.707496,
+            lng: -73.990774
+        }
+    }
+];
