3f400c66e12fb3af0135e823bd32aac0cf0ff204
daniel sc <su7cheng@gmail.com>
2018-12-04 23:25:37 -0200

    Update model.js file.

--- js/model.js
+++ js/model.js
@@ -1,45 +1,143 @@
 /* Model */
 var locations = [
-    {
-        title: 'Empire State Building',
-        location: {
-            lat: 40.748441,
-            lng: -73.985664
-        }
-    },
-    {
-        title: 'Rockefeller Center',
-        location: {
-            lat: 40.758663,
-            lng: -73.981331
-        }
-    },
-    {
-        title: 'Central Park Zoo',
-        location: {
-            lat: 40.767778,
-            lng: -73.971833
-        }
-    },
-    {
-        title: 'Times Square',
-        location: {
-            lat: 40.758895,
-            lng: -73.985131
-        }
-    },
-    {
-        title: 'Japan Society',
-        location: {
-            lat: 40.752524,
-            lng: -73.968287
-        }
-    },
-    {
-        title: 'Manhattan Bridge',
-        location: {
-            lat: 40.707496,
-            lng: -73.990774
-        }
+  {
+    title: 'Empire State Building',
+    location: {
+      lat: 40.748441,
+      lng: -73.985664
     }
+  },
+  {
+    title: 'American Museum of Natural History',
+    location: {
+      lat: 40.781484,
+      lng: -73.973984
+    }
+  },
+  {
+    title: 'The Copacabana Times Square',
+    location: {
+      lat: 40.760459,
+      lng: -73.987248
+    }
+  },
+  {
+    title: 'Carnegie Hall',
+    location: {
+      lat: 40.765353,
+      lng: -73.979902
+    }
+  },
+  {
+    title: 'Central Park Zoo',
+    location: {
+      lat: 40.767778,
+      lng: -73.971833
+    }
+  },
+  {
+    title: 'Times Square',
+    location: {
+      lat: 40.758895,
+      lng: -73.985131
+    }
+  },
+  {
+    title: 'Japan Society',
+    location: {
+      lat: 40.752524,
+      lng: -73.968287
+    }
+  },
+  {
+    title: 'Park Ave Penthouse',
+    location: {
+      lat: 40.7713024,
+      lng: -73.9632393
+    }
+  },
+  {
+    title: 'Chelsea Loft',
+    location: {
+      lat: 40.7444883,
+      lng: -73.9949465
+    }
+  },
+  {
+    title: 'Union Square Open Floor Plan',
+    location: {
+      lat: 40.7347062,
+      lng: -73.9895759
+    }
+  },
+  {
+    title: 'East Village Hip Studio',
+    location: {
+      lat: 40.7281777,
+      lng: -73.984377
+    }
+  },
+  {
+    title: 'TriBeCa Artsy Bachelor Pad',
+    location: {
+      lat: 40.7195264,
+      lng: -74.0089934
+    }
+  },
+  {
+    title: 'Chinatown Homey Space',
+    location: {
+      lat: 40.7180628,
+      lng: -73.9961237
+    }
+  },
+  {
+    title: 'The Basilica of St. Patricks Old Cathedral',
+    location: {
+      lat: 40.723781,
+      lng: -73.994896
+    }
+  },
+  {
+    title: 'New York Stock Exchange',
+    location: {
+      lat: 40.707652,
+      lng: -74.011381
+    }
+  },
+  {
+    title: 'Rockwood Music Hall',
+    location: {
+      lat: 40.722980,
+      lng: -73.988554
+    }
+  },
+  {
+    title: 'Museum of Chinese in America',
+    location: {
+      lat: 40.720263,
+      lng: -73.999207
+    }
+  },
+  {
+    title: 'The Public Theater',
+    location: {
+      lat: 40.729873,
+      lng: -73.991530
+    }
+  },
+  {
+    title: 'United Nations Headquarters',
+    location: {
+      lat: 40.749255,
+      lng: -73.967979
+    }
+  },
+  {
+    title: 'Manhattan Bridge',
+    location: {
+      lat: 40.707496,
+      lng: -73.990774
+      }
+  }
 ];
